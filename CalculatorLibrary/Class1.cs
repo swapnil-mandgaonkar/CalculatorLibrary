5018875ce97d029df31e109f7f80ca698ed0e538
swapnil-mandgaonkar <swapnil.mandgaonkar@yahoo.in>
2015-11-04 23:23:29 +0530

    Initial Cut

---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Calculator
+    {
+        public int add(int a, int b) { return a + b; }
+        public int sub(int a, int b) { return a - b; }
+        public int mult(int a, int b) { return a * b; }
+        public int div(int a, int b) { return a / b; }
+        public int mod(int a, int b) { return a % b; }
+
+    }
+}
